0ffc060141685e060c33fe040c6e00aba8e71274
Lasvad <umlaszcz@myumanitoba.ca>
2015-02-27 13:04:26 -0600

    -Moved login and display Challenger from Application_controller to Welcome_controller
    - display_challenger code could possibly be moved to the challenger controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,3 @@
 class ApplicationController < ActionController::Base
 	
-	# This forces users to the login page
-	before_filter :require_login, :display_challengers
-	
-	protect_from_forgery with: :exception
-	
-	private
-
-	def display_challengers
-		@challengers = Challenger.all
-	end
-
-	def require_login
-		unless cookies[:challenger_name]
-			redirect_to url_for(controller: "/challengers",
-					    action: "new",) and return 
-			
-		end
-	end
 end
