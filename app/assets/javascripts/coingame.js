a1f209c1d116ef8c323fb31fe90aa9167c4f8e15
Lasvad <umlaszcz@myumanitoba.ca>
2015-02-28 17:24:47 -0600

    -tombstoned both coingame.js and update_game.js.erb because they are not used at all. We should consider removal if these are not needed in the future

--- app/assets/javascripts/coingame.js
+++ app/assets/javascripts/coingame.js
@@ -1,18 +1,21 @@
 function coingame(guess) 
 {
-	var x = Math.random()
-	var flip_result
-	var response_to_player
+	//currently not in use
+	//tombstone: 2015-02-28
 	
-	if (x < 0.5)
-		flip_result = "Heads!"
-	else
-		flip_result = "Tails!"
+	// var x = Math.random()
+	// var flip_result
+	// var response_to_player
 	
-	if (flip_result == guess)
-		response_to_player = flip_result + " Win!"
-	else
-		response_to_player = flip_result + " Lose..."
+	// if (x < 0.5)
+	// 	flip_result = "Heads!"
+	// else
+	// 	flip_result = "Tails!"
 	
-	document.getElementById("coin_result").innerHTML = response_to_player;
+	// if (flip_result == guess)
+	// 	response_to_player = flip_result + " Win!"
+	// else
+	// 	response_to_player = flip_result + " Lose..."
+	
+	// document.getElementById("coin_result").innerHTML = response_to_player;
 }
