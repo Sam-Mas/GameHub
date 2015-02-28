8eb764409a2d8dcd3fd955964a98c257ca85b1a1
Shelby Bernhard <umbernhs@myumanitoba.ca>
2015-02-27 18:56:54 -0600

    Cleaned up welcome page UI to move username into header, move account links into a drop down, and move the challenger list into a right slide panel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,13 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+//= require bootstrap.min
+//= require_tree  .
+
+function slideChallengers(show) {
+	if (show) {
+		$('#challengerContainer').addClass("is-visible");
+	} else {
+		$('#challengerContainer').removeClass("is-visible");
+	}
+}
