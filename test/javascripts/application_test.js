a5a8b8498e44c6d09b0e497a9a10c1b1f80f0261
Shelby Bernhard <umbernhs@myumanitoba.ca>
2015-03-11 17:34:53 -0500

    Added a client side test to test slideChallengers(false)

--- test/javascripts/application_test.js
+++ test/javascripts/application_test.js
@@ -1,9 +1,21 @@
 //= require application
 
-QUnit.test("slideChallengers properly adjusts #challengerContainer class", function(assert) {
+function init() {
 	element = document.createElement('div')
 	element.id = 'challengerContainer';
 	document.body.appendChild(element);
+}
+
+QUnit.test("slideChallengers properly adds is-visible class to #challengerContainer element on show=true", function(assert) {
+	init();
+
 	slideChallengers(true);
 	assert.equal($('#challengerContainer').hasClass("is-visible"), true, "slideChallengers did not show");
+});
+
+QUnit.test("slideChallengers properly removes is-visible class to #challengerContainer element show=false", function(assert) {
+	init();
+	
+	slideChallengers(false);
+	assert.equal($('#challengerContainer').hasClass("is-visible"), false, "slideChallengers did not show");
 });
