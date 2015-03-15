f596730146627a1b010f48a45ca6158070161e48
Sam-Mas <samuel.massinon@gmail.com>
2015-03-14 22:43:28 -0500

    Made it so that users can't enter invalid usernames

--- app/controllers/challengers_controller.rb
+++ app/controllers/challengers_controller.rb
@@ -36,44 +36,61 @@ class ChallengersController < ApplicationController
 	end
 
 	def create
-		current_name = cookies[:challenger_name]
-
-		if current_name.include?("Guest")
-			guest = Challenger.find_by_name(current_name)
-			guest.destroy
-		end
 
 		@challenger = Challenger.find_by_name(challenger_params[:name])
+		
+		# if the challenger already exists
+		if @challenger 
+			
+			flash[:danger] = nil
+			flash[:notice] = 'challenger successfully loaded'
+			set_cookies(@challenger)
+			redirect_to root_path
 
-		unless @challenger
+		# try to create the user
+		else
 			@challenger = Challenger.new(challenger_params)
-			@challenger.save
+
+			# on success
+			if @challenger.save
+
+				flash[:danger] = nil
+				flash[:notice] = 'new challenger created'
+				set_cookies(@challenger)
+				redirect_to root_path
+
+			# on failure, ask for another name
+			else
+				flash[:danger] = 'Invalid username, please only user letters and numbers'
+				render 'new'
+			end
 		end
 
-		cookies[:challenger_id] = @challenger.id
-		cookies[:challenger_name] = @challenger.name
-
-		redirect_to root_path
 	end
 
 	def edit
 		@challenger = Challenger.find(cookies[:challenger_id]);
 	end
 
-    def update
-        @challenger = Challenger.find(cookies[:challenger_id])
+	def update
+		@challenger = Challenger.find(cookies[:challenger_id])
 
-        if @challenger.update_attributes(challenger_params)
-            redirect_to root_path
-        else
-            render 'edit'
-        end
-    end
+		if @challenger.update_attributes(challenger_params)
+			redirect_to root_path
+		else
+			render 'edit'
+		end
+	end
 
 	private 
 
 	def challenger_params
 		params.require(:challenger).permit(:name, :balance, :opponent)
+	end
+
+	def set_cookies(challenger)
+		cookies[:challenger_id] = challenger.id
+		cookies[:challenger_name] = challenger.name
 	end
 
 end
---
+++ spec/controllers/challengers_controller_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe ChallengersController, type: :controller do
+
+	context 'when signing in with invalide username' do
+		it 'should tell you it is invalid and keep you on the login page' do
+			post :create, challenger: { name: "" }
+			expect(flash[:danger]).to eq('Invalid username, please only user letters and numbers')
+			expect(response).to render_template(:new)
+		end
+
+		it 'should tell you it is invalid and keep you on the login page' do
+			post :create, challenger: { name: "Guest123456" }
+			expect(flash[:danger]).to eq('Invalid username, please only user letters and numbers')
+			expect(response).to render_template(:new)
+		end
+
+		it 'should tell you it is invalid and keep you on the login page' do
+			post :create, challenger: { name: "_" }
+			expect(flash[:danger]).to eq('Invalid username, please only user letters and numbers')
+			expect(response).to render_template(:new)
+		end
+
+		it 'should tell you it is invalid and keep you on the login page' do
+			post :create, challenger: { name: "Bad_Username" }
+			expect(flash[:danger]).to eq('Invalid username, please only user letters and numbers')
+			expect(response).to render_template(:new)
+		end
+	end
+
+	context 'when signing in with valide username' do
+		it 'should tell you it is valide and redirct you to main page' do
+			post :create, challenger: { name: "Sam" }
+			expect(flash[:notice]).to be_present
+			expect(response).to redirect_to(root_path)
+		end
+
+		it 'should tell you it is invalid and keep you on the login page' do
+			post :create, challenger: { name: "Sam" }
+			expect(flash[:notice]).to eq('new challenger created')
+			expect(response).to redirect_to(root_path)
+		end
+
+		it 'should tell you it is invalid and keep you on the login page' do
+			test = Challenger.new
+			test.name = "Sam"
+			test.save
+			post :create, challenger: { name: "Sam" }
+			expect(flash[:notice]).to eq('challenger successfully loaded')
+			expect(response).to redirect_to(root_path)
+		end
+	end
+
+end
