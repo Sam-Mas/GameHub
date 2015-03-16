61ccbd4487c0efdf00750f523a6df84b131bc2ba
Sam-Mas <samuel.massinon@gmail.com>
2015-03-16 00:32:41 -0500

    Forgot to save this file before commiting

--- app/models/game/coin_game.rb
+++ app/models/game/coin_game.rb
@@ -48,30 +48,6 @@ class Game::CoinGame < ActiveRecord::Base
 		return message
 
 	end
-
-
-	def who_won(challenger_id)
-		@message = "You Lost"
-		
-		if self.challengers.first.id == challenger_id
-			
-			if self.score1 > self.score2
-				@message = "You Won"
-			elsif self.score1 == self.score2
-				@message = "A Tie"
-			end
-
-		elsif self.challengers.last.id == challenger_id
-			if self.score1 < self.score2
-				@message = "You Won"
-			elsif self.score1 == self.score2
-				@message = "A Tie"
-			end
-		end
-
-		return @message
-
-	end
 	
 	def get_score_for(challenger_id)
 	
