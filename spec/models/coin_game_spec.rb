9d9f4a2955ba40d81aa8479c41974c52bbd3a922
anonymous <anonymous@myumanitoba.ca>
2015-03-17 17:32:11 -0500

    Additional coin_game model tests
    6 tests and additional logic to set up a mock game while avoiding
    cookies

--- spec/models/coin_game_spec.rb
+++ spec/models/coin_game_spec.rb
@@ -3,14 +3,44 @@ require 'rails_helper'
 RSpec.describe Game::CoinGame, type: :model do
 
 	context 'flip logic' do
-		before :each do
-			@guest = Challenger.create(name: "Bob")
+		before :each do #set up a dummy game
+			@player1 = Challenger.create(name: "Bob")
+			@player2 = Challenger.create(name: "Jim")
 			@game = Game::CoinGame.new
+			@game.challengers << @player1
+			@game.challengers << @player2
 		end
 
 		it "loads this test correctly" do
 			expect(@game).to be_instance_of(Game::CoinGame)
 		end
+
+		it "checks if the game is active once created" do
+			expect(@game.done?()).to be false
+		end
+		
+		it "checks player 1's self-identity" do
+			expect(@game.get_me(@player1.id)).to be == (@player1)
+		end
+		
+		it "checks player 2's self-identity" do
+			expect(@game.get_me(@player2.id)).to be == (@player2)
+		end
+
+		it "checks player 1's opponent's identity" do
+			expect(@game.get_opponent(@player1.id)).to be == (@player2)
+		end
+		
+		it "checks player 2's opponent's identity" do
+			expect(@game.get_opponent(@player2.id)).to be == (@player1)
+		end		
+
+
+
+			
+		
+
+		
 
 	end
 
